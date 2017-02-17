65c6f2259edf283df46f65c4f5d9ff0156f97eff
Sondre <songro93@gmail.com>
2017-02-16 16:55:31 -0800

    imported code form lecture, rewrote it a bit to suit my style and finished task1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(174, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Default"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(156, 342)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(174, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Order by forename"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(308, 369)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,49 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private serverName As String = "mysql.stud.iie.ntnu.no"
+    Private databaseName As String = "sondg"
+    Private userID As String = "sondg"
+    Private password As String = "7ppasexr"
+    Private connString As String = String.Format("Server={0}; Database={1}; Uid={2}; Pwd={3};", serverName, databaseName, userID, password)
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        connectAndWrite("SELECT * FROM personer")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        connectAndWrite("SELECT * FROM personer ORDER BY fornavn")
+    End Sub
+
+    Private Sub connectAndWrite(query As String)
+        Dim connection As New MySqlConnection(connString)
+        Try
+            connection.Open()
+            Console.WriteLine("Connecting with no issues.")
+            'Lager sentrale objekter
+            Dim sql As New MySqlCommand(query, connection)
+            Dim dataAdapter As New MySqlDataAdapter
+            Dim internalTable As New DataTable
+            'Objektet "da" utfører spørringen og legger resultatet i "interntabell"
+            dataAdapter.SelectCommand = sql
+            dataAdapter.Fill(internalTable)
+            'Har ikke lenger bruk for å være tilkoblet til databasen
+            connection.Close()
+            'En tabell har mange rader. DataRow-objektet kan lagre 1 rad om gangen
+            Dim rad As DataRow
+            Dim personid, fornavn, etternavn As String 'hjelpevariabler
+            'Fyller listeboksen med ønsket informasjon
+            ListBox1.Items.Clear() 'fjerner først eventuell gammel tekst
+            For Each rad In internalTable.Rows
+                personid = rad("id")
+                fornavn = rad("fornavn")
+                etternavn = rad("etternavn")
+                ListBox1.Items.Add(personid & " " & fornavn & " " & etternavn)
+            Next rad
+        Catch feilmelding As MySqlException
+            Console.WriteLine("Feil ved tilkobling til databasen: " &
+            feilmelding.Message)
+        Finally
+            connection.Dispose()
+        End Try
+    End Sub
 End Class
